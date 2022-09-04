a9b1c17b130b824aa35c0fd128615316107601ab
Jonas <jonasexplore@gmail.com>
2022-09-04 12:04:16 -0300

    chore: initial project

---
+++ src/main/java/com/fallying/crmapi/CrmApiApplication.java
@@ -0,0 +1,13 @@
+package com.fallying.crmapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmApiApplication.class, args);
+	}
+
+}
